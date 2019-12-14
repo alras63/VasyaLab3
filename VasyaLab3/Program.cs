a84e7a601ef81b530d5538415dae8d87396a9ab8
ural1 <ural1@DESKTOP-EKUFV44>
2019-12-14 13:25:18 +0400

    Добавьте файлы проекта.

---
+++ VasyaLab3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VasyaLab3
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+  
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(ofd.FileName);
+
+              
+
+                string line;
+
+                while((line = sr.ReadLine()) != null)
+                {
+                    string[] words = line.Split(new char[] { ' ' });
+
+                    for (int i = 0; i < words.Length - 1; i += 2)
+                    {
+                        // Переставляем местами.
+                        Console.Write(words[i + 1] + " " + words[i] + " ");
+                    }
+
+                    if (words.Length % 2 != 0)
+                    {
+                        Console.WriteLine(words[words.Length - 1]);
+
+                    }
+                }
+               
+            }
+            else
+            {
+                Console.WriteLine("Произошла ошибка");
+            }
+            Console.Read();
+
+        }
+    }
+}
